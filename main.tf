bca52ba8622260ec8c0865d8a7545de887e528e8
thecosmicninja <74620023+thecosmicninja@users.noreply.github.com>
2020-11-17 19:34:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "elb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/31"]
   }
 
   # outbound internet access
